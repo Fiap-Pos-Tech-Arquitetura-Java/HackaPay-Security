8fafe7f0848c9ed73c31e4ec2baee9522a689ac0
Anderson0Wagner <anderson.wagner@gmail.com>
2024-07-28 20:25:52 -0300

    inicilizacao do projeto com spring

---
+++ src/main/java/br/com/fiap/postech/hackapay/HackaPaySecurityApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.postech.HackaPay_Security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackaPaySecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackaPaySecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/postech/hackapay/HackaPaySecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.postech.HackaPay_Security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackaPaySecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
