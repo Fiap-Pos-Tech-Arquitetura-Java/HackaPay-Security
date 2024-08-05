5bbf0e6cc0a3e47ac9915863207d4c90e9d3e945
andersonwagner <anderson.wagner@gmail.com>
2024-08-05 23:26:22 +0200

    adicionando logs para investigacao de problema de 403.

--- src/main/java/br/com/fiap/postech/hackapay/security/UserDetailsServiceImpl.java
+++ src/main/java/br/com/fiap/postech/hackapay/security/UserDetailsServiceImpl.java
@@ -2,6 +2,7 @@ package br.com.fiap.postech.hackapay.security;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -18,9 +19,18 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 	@Override
 	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
 		RestClient restClient = RestClient.create();
+		var uri = baseURI + "/findByLogin/{userName}";
+		System.out.println("uri = " + uri);
 		User user = restClient.get()
-				.uri(baseURI + "/findByLogin/{userName}", userName)
+				.uri(uri, userName)
 				.retrieve()
+				.onStatus(HttpStatusCode::is4xxClientError, (request, response) -> {
+					System.out.println("statusCode = "+ response.getStatusCode() + " statusText = "+ response.getStatusText());
+					throw new IllegalArgumentException("Falha na busca do usuario pelo login: ");
+				}).onStatus(HttpStatusCode::is5xxServerError, (request, response) -> {
+					System.out.println("statusCode = "+ response.getStatusCode() + " statusText = "+ response.getStatusText());
+					throw new IllegalArgumentException("Falha na busca do usuario pelo login 2 ..");
+				})
 				.body(User.class);
 		if (user != null) {
 			return new UserDetailsImpl(user);
